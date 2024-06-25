1c77b578fc883ce5cabf421d3daedc6bc97a6a21
happy0412 <siphahle.nomnganga@gmail.com>
2024-06-25 13:29:26 +0200

    Setting up Test hooks

--- cypress/integration/examples/Test.js
+++ cypress/integration/examples/Test.js
@@ -8,13 +8,19 @@ describe('My First Test', function()
 
         //Runs once before all tests in the block
 
+        cy.fixture('example').then(function(data)
+        {
+            
+            this.data = data
+        })
+
     })
 
 
     it('Does not do much!', function(){
        
         cy.visit("https://rahulshettyacademy.com/angularpractice/")
-        cy.get(':nth-child(1) > .form-control').type("Siphahle")
-        cy.get('select').select("Female")
+        cy.get(':nth-child(1) > .form-control').type(this.data.name)
+        cy.get('select').select(this.data.gender)
     })
 })
