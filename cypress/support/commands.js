0eb3facb0e6f4c773a3cf011ddd4680b144c9e47
happy0412 <siphahle.nomnganga@gmail.com>
2024-06-25 21:43:25 +0200

    added ProductPage Objec and HomePageObject

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -12,9 +12,7 @@
 // -- This is a parent command --
  Cypress.Commands.add('selectProduct', (productName) => {
     cy.get('h4.card-title').each(($el,index, $list) =>{
-
-
-        if($el.text().includes('Blackberry')) 
+        if($el.text().includes(productName)) 
          {
              cy.get('button.btn.btn-info').eq(index).click()
          }
